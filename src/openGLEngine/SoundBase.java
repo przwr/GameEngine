bc5d6b1faa3f7644f2cedfa7a6f11233dad23e0d
Wojciech Drazkiewicz <troll12345@wp.pl>
2014-10-01 23:40:18 +0200

    Muzyka i dźwięki

--- src/openGLEngine/SoundBase.java
+++ src/openGLEngine/SoundBase.java
@@ -8,15 +8,8 @@ package openGLEngine;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import org.lwjgl.LWJGLException;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.openal.AL;
 import org.newdawn.slick.openal.Audio;
 import org.newdawn.slick.openal.AudioLoader;
-import org.newdawn.slick.openal.SoundStore;
 import org.newdawn.slick.util.ResourceLoader;
 
 /**
