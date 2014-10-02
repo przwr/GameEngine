f756c19f3bb7f894249f980a31867566a11986a8
wrobelp <wrobelp@op.pl>
2014-10-03 01:40:43 +0200

    Drobne Poprawki Światła - powinno być Ok!

--- src/game/place/Light.java
+++ src/game/place/Light.java
@@ -21,28 +21,27 @@ public class Light {
     protected float r;
     protected float g;
     protected float b;
+    protected int strength;
 
-    public Light(String lightName, float r, float g, float b, int sx, int sy) {
+    public Light(String lightName, float r, float g, float b, int strength, int sx, int sy) {
         this.r = r;
         this.g = g;
         this.b = b;
+        this.strength = strength;
         this.light = new Sprite(lightName, sx, sy);
     }
 
     public void render(GameObject emitter, Place place, Camera cam) {
         glColor3f(r - place.r, g - place.g, b - place.b);
-        //float val = 0.8f - place.r;
-        //glColor3f(val, val, val);
-        //glColor3f(r - 2f * place.r, g - 2f * place.g, b - 2f * place.b);
-        //glColor3f(r - 1.5f * place.r, g - 1.5f * place.g, b - 1.5f * place.b);
-        //glColor3f(r, g, b);
         glPushMatrix();
         if (emitter.getClass() == Player.class) {
             glTranslatef(emitter.getMidX() - light.getSx() / 2 - place.getXOff(((Player) emitter).getCam()) + place.getXOff(cam), emitter.getMidY() - light.getSy() / 2 - place.getYOff(((Player) emitter).getCam()) + place.getYOff(cam), 0);
         } else {
             glTranslatef(emitter.getMidX() - light.getSx() / 2, emitter.getMidY() - light.getSy() / 2, 0);
         }
-        light.render();
+        for (int i = 0; i < strength; i++) {
+            light.render();
+        }
         glPopMatrix();
     }
 
@@ -57,13 +56,17 @@ public class Light {
         this.b = b;
     }
 
+    public void setStrength(int strenght) {
+        this.strength = strenght;
+    }
+
     public void render(GameObject emitter, Place place, int x, int y) {
         glColor3f(r - place.r, g - place.g, b - place.b);
-        //glColor3f(r - 1.5f * place.r, g - 1.5f * place.g, b - 1.5f * place.b);
-        //glColor3f(r, g, b);
         glPushMatrix();
         glTranslatef(emitter.getMidX() - light.getSx() / 2 + x, emitter.getMidY() - light.getSy() / 2 + y, 0);
-        light.render();
+        for (int i = 0; i < strength; i++) {
+            light.render();
+        }
         glPopMatrix();
     }
 }
