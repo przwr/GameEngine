003720369835068e76d0e7ce9530b685db71b59f
Przemysław Wróbel <wrobelp@op.pl>
2015-06-17 22:18:54 +0200

    Króliki są coraz mądrzejsze, do naprawienia: - isFittingPoint w PathFinder

--- src/game/gameobject/Controler.java
+++ src/game/gameobject/Controler.java
@@ -14,12 +14,32 @@ public abstract class Controler {
     public Action[] actions;
     public AnyInput[] inputs;
     protected final Entity inControl;
-    protected boolean[] states;
+    protected byte[] states;
+    
+    public static final byte KEY_NO_INPUT = 0, KEY_PRESSED = 1, KEY_CLICKED = 2, KEY_RELEASED = -1;
 
     public Controler(Entity inControl) {
         this.inControl = inControl;
     }
+    
+    public void showInput() {
+        for (byte b : states)
+            System.out.print(b + " ");
+        System.out.println("");
+    }
 
+    public boolean isKeyReleased(int key) {
+        return states[key] == KEY_RELEASED;
+    }
+    
+    public boolean isKeyClicked(int key) {
+        return states[key] == KEY_CLICKED;
+    }
+    
+    public boolean isKeyPressed(int key) {
+        return states[key] > KEY_NO_INPUT;
+    }    
+    
     public abstract void getInput();
 
     public abstract boolean isMenuOn();
