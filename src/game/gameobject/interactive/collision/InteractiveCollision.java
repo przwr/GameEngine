ee14dc18029837ed5efa5d5f69ddccf79386e665
Domi <Domi@192.168.1.13>
2015-11-07 14:39:34 +0100

    UWAGA! Dodałem renderowanie kolizji Interactive. Renderują się tylko jako są aktywne. W Main ustawia się flagę RENDER_INTERACTIVE_COLLISION Nie jestem pewien, czy to jest na 100% dokładnie rysowane, ale wygląda OK. Poprawienie błędu w LineInteractiveCollision

---
+++ src/game/gameobject/interactive/collision/InteractiveCollision.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.gameobject.interactive.collision;
+
+import collision.Figure;
+import engine.utilities.Methods;
+import engine.utilities.Point;
+import game.gameobject.GameObject;
+import game.gameobject.entities.Player;
+import game.gameobject.interactive.InteractiveResponse;
+
+import java.awt.geom.Line2D;
+
+import static game.gameobject.GameObject.*;
+
+/**
+ * @author przemek
+ */
+public abstract class InteractiveCollision {
+
+	private static int[] directions = {InteractiveResponse.FRONT, InteractiveResponse.FRONT, InteractiveResponse.SIDE, InteractiveResponse.BACK,
+		InteractiveResponse.BACK, InteractiveResponse.BACK, InteractiveResponse.SIDE, InteractiveResponse.FRONT};
+	protected Point position = new Point();
+	protected int fromBottom, height, shift;
+	protected InteractiveResponse response = new InteractiveResponse();
+
+	public InteractiveCollision(int fromBottom, int height, int shift) {
+		this.fromBottom = fromBottom;
+		this.height = height;
+		this.shift = shift;
+	}
+
+	protected static int circleToCircleDistance(int xA, int yA, int xB, int yB, int radiusA, int radiusB) {
+		return (radiusA + radiusB) - Methods.pointDistance(xA, (int) (yA * Methods.SQRT_ROOT_OF_2), xB, (int) (yB * Methods.SQRT_ROOT_OF_2));
+	}
+
+	protected static int lineToCircleDistance(int xC, int yC, int radius, Point start, Point end, int length) {
+		double d = Line2D.ptSegDist(start.getX(), (int) (start.getY() * Methods.SQRT_ROOT_OF_2), end.getX(), (end.getY() * Methods.SQRT_ROOT_OF_2), xC, (int) (yC * Methods.SQRT_ROOT_OF_2));
+		if (d <= radius) {
+			int e = Methods.pointDistance(xC, (int) (yC * Methods.SQRT_ROOT_OF_2), end.getX(), (int) (end.getY() * Methods.SQRT_ROOT_OF_2));
+			if (d < 1) {
+				int pixelsIn = radius + e;
+				return pixelsIn > length ? length : pixelsIn;
+			} else {
+				double d2 = d * d;
+				int x = (int) Math.sqrt(e * e - d2);
+				int y = (int) Math.sqrt(radius * radius - d2);
+				if (x < y) {
+					return y > length ? length : y;
+				}
+				int pixelsIn = x + y;
+				return pixelsIn > length ? length : pixelsIn;
+			}
+		}
+		return -1;
+	}
+
+	public abstract void updatePosition(GameObject owner);
+
+	protected abstract InteractiveResponse collideImplementation(GameObject owner, GameObject object, byte attackType);
+
+	protected abstract InteractiveResponse collideImplementation(GameObject owner, Player player, byte attackType);
+
+	public InteractiveResponse collide(GameObject owner, GameObject object, byte attackType) {
+		if (owner.getCollision().isCollide() && object.getCollision().isHitable()) {
+			return collideImplementation(owner, object, attackType);
+		} else {
+			return InteractiveResponse.NO_RESPONSE;
+		}
+	}
+
+	public InteractiveResponse collide(GameObject owner, Player player, byte attackType) {
+		if (owner.getCollision().isCollide() && player.getCollision().isHitable()) {
+			return collideImplementation(owner, player, attackType);
+		} else {
+			return InteractiveResponse.NO_RESPONSE;
+		}
+	}
+
+	protected int calculateInteractionDirection(int objectDirection, Figure collision, int x, int y) {
+		int xS = collision.getX();
+		int xE = collision.getXEnd();
+		int yS = collision.getY();
+		int yE = collision.getYEnd();
+		int ownerDirection;
+		if (x > xE) {
+			if (y > yE) {
+				ownerDirection = DOWN_RIGHT;
+			} else if (y > yS) {
+				ownerDirection = RIGHT;
+			} else {
+				ownerDirection = UP_RIGHT;
+			}
+		} else if (x > xS) {
+			if (y > yE) {
+				ownerDirection = DOWN;
+			} else if (y > yS) {   // point inside
+				ownerDirection = objectDirection;
+			} else {
+				ownerDirection = UP;
+			}
+		} else {
+			if (y > yE) {
+				ownerDirection = DOWN_LEFT;
+			} else if (y > yS) {
+				ownerDirection = LEFT;
+			} else {
+				ownerDirection = UP_LEFT;
+			}
+		}
+		return directions[Math.abs(objectDirection - ownerDirection)];
+	}
+
+	public abstract void render(GameObject owner, int xEffect, int yEffect);
+}
--- src/game/gameobject/items/Arrow.java
+++ src/game/gameobject/items/Arrow.java
@@ -8,20 +8,22 @@ package game.gameobject.items;
 import collision.Figure;
 import collision.OpticProperties;
 import collision.Rectangle;
+import engine.Main;
 import engine.utilities.Drawer;
 import engine.utilities.Methods;
 import game.gameobject.GameObject;
 import game.gameobject.entities.Entity;
 import game.gameobject.entities.Player;
-import game.gameobject.interactive.CircleInteractiveCollision;
+import game.gameobject.interactive.collision.CircleInteractiveCollision;
 import game.gameobject.interactive.Interactive;
-import game.gameobject.interactive.UpdateBasedActivator;
+import game.gameobject.interactive.activator.UpdateBasedActivator;
 import game.gameobject.stats.Stats;
 import game.place.Place;
 import net.packets.Update;
 import org.newdawn.slick.Color;
 
 import static game.gameobject.interactive.Interactive.BOW_HURT;
+import game.gameobject.interactive.collision.InteractiveCollision;
 import static game.gameobject.items.Weapon.BOW;
 import static org.lwjgl.opengl.GL11.*;
 
@@ -30,125 +32,130 @@ import static org.lwjgl.opengl.GL11.*;
  */
 public class Arrow extends Entity {
 
-    private final GameObject owner;
-    private boolean stopped;
+	private final GameObject owner;
+	private boolean stopped;
 
-    public Arrow(double speed, int direction, int height, GameObject owner) {
-        this.floatHeight = height;
-        this.xSpeed = Methods.xRadius(direction, speed);
-        this.ySpeed = -Methods.yRadius(direction, speed);
-        this.owner = owner;
-        setDirection(direction);
-        setCollision(Rectangle.create(Place.tileHalf, Place.tileHalf, OpticProperties.NO_SHADOW, this));
-        visible = true;
-        stats = new Stats(this);
-        stats.setStrength(20);
-        Interactive attack = Interactive.create(this, new UpdateBasedActivator(), new CircleInteractiveCollision(0, 64, -24, 64), BOW_HURT, BOW, (byte) 1, 1f);
-        attack.addException(owner);
-        addInteractive(attack);
-    }
+	public Arrow(double speed, int direction, int height, GameObject owner) {
+		this.floatHeight = height;
+		this.xSpeed = Methods.xRadius(direction, speed);
+		this.ySpeed = -Methods.yRadius(direction, speed);
+		this.owner = owner;
+		setDirection(direction);
+		setCollision(Rectangle.create(Place.tileHalf, Place.tileHalf, OpticProperties.NO_SHADOW, this));
+		visible = true;
+		stats = new Stats(this);
+		stats.setStrength(20);
+		Interactive attack = Interactive.create(this, new UpdateBasedActivator(), new CircleInteractiveCollision(0, 64, -24, 64), BOW_HURT, BOW, (byte) 1, 1f);
+		attack.addException(owner);
+		addInteractive(attack);
+	}
 
-    @Override
-    public void updateOnline() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public void updateOnline() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 
-    @Override
-    protected void updateRest(Update update) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	protected void updateRest(Update update) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 
-    @Override
-    protected boolean isCollided(double xMagnitude, double yMagnitude) {
-        boolean ret = collision.isCollideSolid((int) (getXInDouble() + xMagnitude), (int) (getYInDouble() + yMagnitude), map);
-        if (ret) {
-            stopped = true;
-            getAttackActivator().setActivated(true);
-        }
-        return ret;
-    }
+	@Override
+	protected boolean isCollided(double xMagnitude, double yMagnitude) {
+		boolean ret = collision.isCollideSolid((int) (getXInDouble() + xMagnitude), (int) (getYInDouble() + yMagnitude), map);
+		if (ret) {
+			stopped = true;
+			getAttackActivator().setActivated(true);
+		}
+		return ret;
+	}
 
-    @Override
-    public void reactToAttack(byte attackType, GameObject attacked) {
-        delete();
-    }
+	@Override
+	public void reactToAttack(byte attackType, GameObject attacked) {
+		delete();
+	}
 
-    @Override
-    protected Player getCollided(double xMagnitude, double yMagnitude) {
-        return null;
-    }
+	@Override
+	protected Player getCollided(double xMagnitude, double yMagnitude) {
+		return null;
+	}
 
-    @Override
-    public void render(int xEffect, int yEffect) {
-        int delta;
-        if (!stopped) {
-            moveIfPossibleWithoutSliding(xSpeed + xEnvironmentalSpeed, ySpeed + yEnvironmentalSpeed);
-            updateWithGravity();
-            if (floatHeight == 0) {
-                stopped = true;
-            }
-        } else if ((delta = Methods.pointDifference(getX(), getY(), owner.getX(), owner.getY())) < Place.tileSize) {
-            setPosition(x + (owner.getX() - x) / 5, y + (owner.getY() - y) / 5);
-            if (delta < Place.tileHalf / 2) {
-                delete();
-            }
-        }
-        glPushMatrix();
-        glTranslatef(xEffect, yEffect, 0);
-        glScaled(Place.getCurrentScale(), Place.getCurrentScale(), 1);
-        glTranslatef(getX(), getY(), 0);
-        Drawer.setColor(JUMP_SHADOW_COLOR);
-        Drawer.drawEllipse(0, 0, Methods.roundDouble(collision.getWidth() * Place.getCurrentScale() / 2f), Methods.roundDouble(collision.getHeight()
-                * Place.getCurrentScale() / 2f), 24);
-        Drawer.setColor(Color.black);
-        Drawer.drawEllipse(0, (int) -floatHeight, Methods.roundDouble(collision.getWidth() * Place.getCurrentScale() / 2f), Methods.roundDouble(collision
-                .getHeight() * Place.getCurrentScale() / 2f), 24);
-        Drawer.refreshColor();
-        glPopMatrix();
-    }
+	@Override
+	public void render(int xEffect, int yEffect) {
+		int delta;
+		if (!stopped) {
+			moveIfPossibleWithoutSliding(xSpeed + xEnvironmentalSpeed, ySpeed + yEnvironmentalSpeed);
+			updateWithGravity();
+			if (floatHeight == 0) {
+				stopped = true;
+			}
+		} else if ((delta = Methods.pointDifference(getX(), getY(), owner.getX(), owner.getY())) < Place.tileSize) {
+			setPosition(x + (owner.getX() - x) / 5, y + (owner.getY() - y) / 5);
+			if (delta < Place.tileHalf / 2) {
+				delete();
+			}
+		}
+		glPushMatrix();
+		glTranslatef(xEffect, yEffect, 0);
+		glScaled(Place.getCurrentScale(), Place.getCurrentScale(), 1);
+		glTranslatef(getX(), getY(), 0);
+		Drawer.setColor(JUMP_SHADOW_COLOR);
+		Drawer.drawEllipse(0, 0, Methods.roundDouble(collision.getWidth() * Place.getCurrentScale() / 2f), Methods.roundDouble(collision.getHeight()
+				* Place.getCurrentScale() / 2f), 24);
+		Drawer.setColor(Color.black);
+		Drawer.drawEllipse(0, (int) -floatHeight, Methods.roundDouble(collision.getWidth() * Place.getCurrentScale() / 2f), Methods.roundDouble(collision
+				.getHeight() * Place.getCurrentScale() / 2f), 24);
+		Drawer.refreshColor();
+		glPopMatrix();
+		if (Main.SHOW_INTERACTIVE_COLLISION) {
+			interactiveObjects.stream().forEach((interactive) -> {
+				interactive.render(xEffect, yEffect);
+			});
+		}
+	}
 
-    @Override
-    public void renderShadowLit(int xEffect, int yEffect, Figure figure) {
-        if (appearance != null) {
-            glPushMatrix();
-            glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
-            Drawer.drawShapeInShade(appearance, 1);
-            glPopMatrix();
-        }
-    }
+	@Override
+	public void renderShadowLit(int xEffect, int yEffect, Figure figure) {
+		if (appearance != null) {
+			glPushMatrix();
+			glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
+			Drawer.drawShapeInShade(appearance, 1);
+			glPopMatrix();
+		}
+	}
 
-    @Override
-    public void renderShadow(int xEffect, int yEffect, Figure figure) {
-        if (appearance != null) {
-            glPushMatrix();
-            glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
-            Drawer.drawShapeInBlack(appearance);
-            glPopMatrix();
-        }
-    }
+	@Override
+	public void renderShadow(int xEffect, int yEffect, Figure figure) {
+		if (appearance != null) {
+			glPushMatrix();
+			glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
+			Drawer.drawShapeInBlack(appearance);
+			glPopMatrix();
+		}
+	}
 
-    @Override
-    public void renderShadowLit(int xEffect, int yEffect, int xStart, int xEnd) {
-        if (appearance != null) {
-            glPushMatrix();
-            glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
-            Drawer.drawShapePartInShade(appearance, 1, xStart, xEnd);
-            glPopMatrix();
-        }
-    }
+	@Override
+	public void renderShadowLit(int xEffect, int yEffect, int xStart, int xEnd) {
+		if (appearance != null) {
+			glPushMatrix();
+			glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
+			Drawer.drawShapePartInShade(appearance, 1, xStart, xEnd);
+			glPopMatrix();
+		}
+	}
 
-    @Override
-    public void renderShadow(int xEffect, int yEffect, int xStart, int xEnd) {
-        if (appearance != null) {
-            glPushMatrix();
-            glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
-            Drawer.drawShapePartInBlack(appearance, xStart, xEnd);
-            glPopMatrix();
-        }
-    }
+	@Override
+	public void renderShadow(int xEffect, int yEffect, int xStart, int xEnd) {
+		if (appearance != null) {
+			glPushMatrix();
+			glTranslatef(getX() + xEffect, getY() + yEffect - (int) floatHeight, 0);
+			Drawer.drawShapePartInBlack(appearance, xStart, xEnd);
+			glPopMatrix();
+		}
+	}
 
-    public GameObject getOwner() {
-        return owner;
-    }
+	public GameObject getOwner() {
+		return owner;
+	}
 
 }
