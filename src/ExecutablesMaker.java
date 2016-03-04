a38cf3b0f1f9054e46aa0350ecefeae0877b0feb
Przemysław Wróbel <wrobelp@op.pl>
2016-03-04 16:29:09 +0100

    MILESTONE - Crossroads - Prototype 03.2016

--- src/ExecutablesMaker.java
+++ src/ExecutablesMaker.java
@@ -3,14 +3,17 @@
  */
 
 import org.ninjacave.jarsplice.core.ShellScriptSplicer;
+import org.ninjacave.jarsplice.core.Splicer;
 import org.ninjacave.jarsplice.core.WinExeSplicer;
 
 public class ExecutablesMaker {
     public static void main(String[] args) {
 //        UWAGA! Gra musi być najpierw skompilowana do .jar do folderu root (patrz niżej). NetBeans robi to automatycznie.
 
+
         ShellScriptSplicer shellScriptSplicer = new ShellScriptSplicer();
         WinExeSplicer winExeSplicer = new WinExeSplicer();
+        Splicer splicer = new Splicer();
 
         String root = "dist/";
         String[] jars = {root + "GameEngine.jar", root + "lib/jinput.jar",
@@ -19,13 +22,14 @@ public class ExecutablesMaker {
                 root + "lib/lwjgl-natives.jar", root + "lib/lwjgl.jar",
                 root + "lib/lwjgl_util.jar", root + "lib/slick-util.jar"};
         String[] natives = new String[0];
-        String output = "Gra.";
+        String output = "data";
         String mainClass = "engine.Launcher";
-        String vmArgs = "-XX:+UseCompressedOops -Xmx1024M";
+        String vmArgs = "";
 
         try {
-            shellScriptSplicer.createFatJar(jars, natives, output + "sh", mainClass, vmArgs);
-            winExeSplicer.createFatJar(jars, natives, output + "exe", mainClass, vmArgs);
+            splicer.createFatJar(jars, natives, output + ".crj", mainClass, vmArgs);
+//            shellScriptSplicer.createFatJar(jars, natives, output + ".sh", mainClass, vmArgs);
+//            winExeSplicer.createFatJar(jars, natives, output + ".exe", mainClass, vmArgs);
         } catch (Exception e) {
             System.out.println("Nie udało się stworzyć plików wykonywalnych:(\nBŁĘDY:");
             e.printStackTrace();
@@ -34,6 +38,7 @@ public class ExecutablesMaker {
 
         String currentDirectory = System.getProperty("user.dir");
         String gameFiles = currentDirectory + "/" + output;
-        System.out.println("Pliki wykonywalne gry stworzono! <('_')>\nPLIKI:\n" + gameFiles + "sh\n" + gameFiles + "exe");
+//        System.out.println("Pliki wykonywalne gry stworzono! <('_')>\nPLIKI:\n" + gameFiles + ".sh\n" + gameFiles + ".exe");
+        System.out.println("Plik wykonywalny gry stworzono! <('_')>\nPLIKI:\n" + gameFiles + ".crj");
     }
 }
